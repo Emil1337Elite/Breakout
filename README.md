2982500c2ae88ba42bf4dd730d0199f33d2729c1
Emil1337Elite <emiloster.katt@gmail.com>
2022-05-15 16:58:28 +0200

    Completedt readme and added comments

--- README.md
+++ README.md
@@ -6,15 +6,50 @@ This project is written in Python and the purpose is to recreate the classic arc
 
 ## Built with
 
-Python
+### Languages
 
-Markdown
+Python - the project itself is written in Python.
+
+Markdown - the readme is written with markdown.
+
+### Libraries
+
+pygame - used to display graphics and run the program
 
 ## Requirements
 
+- Pygame 3.7+
+
+- pygame module
+
 ## Installation
 
+This project is tested in Python 3.7+. To install Python yo can visit (https://www.python.org/downloads/)
+
+The program also requires the pygame library to be installed. Python 3.7+ is required to install pygame, followed by the following command written in the Command Prompt:
+
+```cmd
+pip install pygame
+```
+
 ## Code conventions
+
+- Follows PEP-8 standard
+
+- Variables and functions with multiple words are seperated with underscores to improve readability (snake case).
+
+- Classes are written in camel case.
+
+## Usage
+
+Press run to run the program, a new window will then open and display the game. The platform is controlled by moving the mouse. The game is over when all blocks are gone or the ball hits the bottom of the screen.
+
+## Example
+
+- How the game looks while running
+
+<img width="600" alt="image" src="https://user-images.githubusercontent.com/94603590/168470496-75607dfd-f2fb-4f83-b973-084af1a24d2b.PNG">
+
 
 ## To do
 
@@ -35,18 +70,91 @@ Markdown
 
 ## Changelog
 
-### Version 0.1
+### Version 1.0.0
 
 #### Changed or added
 
-Sprites for ball, blocks and player controlled platform.
+Added sprites for ball and blocks.
+
+***
+
+### Version 1.0.1
+
+#### Changed or added
+
+Added sprite for player platform.
+
+***
+
+### Version 1.0.2
+
+#### Changed or added
+
+Added platform movement
+
+Changed controls from the arrow keys to mouse control
+
+***
+### Version 1.0.3
+
+#### Changed or added
+
+Added collision on the ball, walls and platform
+
+Added ball movement
+
+***
+
+### Version 1.0.4
+
+#### Changed or added
+
+Added a game over screen
+
+Added the graphic for the score counter
+
+***
+
+### Version 1.0.5
+
+#### Changed or added
+
+Added functioning score counter
+
+Added block collision 
+
+Added win state and screen when all blocks are gone
+
+***
+
+### Version 1.0.6
+
+#### Changed or added
+
+Added a highscore function that reads the highscore from a file
+
+***
+
+### Version 1.0.7
+
+#### Changed or added
+
+Fixed a bug where the ball would get stuck bouncing vertically
 
 ***
 
 ## Contribution
 
+No contributions allowed.
+
 ## License
+
+[MIT](https://choosealicense.com/licenses/mit/)
 
 ## Contact
 
+Emil Ahrnstedt Öster - Discord: CRUST#7392 - emil.osterahrnstedt@gmail.com
+
 ## Acknowledgements
+
+[Tutorial for how to add sprites in pygame](https://www.youtube.com/watch?v=hDu8mcAlY4E)
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import pygame
 from random import randint
 
-#Define colors to be used
+#Defines colors to be used
 orange = (255,128,0)
 white = (255,255,255)
 black = (0,0,0)
@@ -14,11 +14,17 @@ life = 1
 block_width = 23
 block_height = 15
 
-#This class represents the blocks in the game
 class Block(pygame.sprite.Sprite):
 
 #Constructor, takes in color and the x and y position
     def __init__(self,color,x,y):
+        """The constructor block class
+
+        Args:
+            color (tuple): The color of the blocks
+            x (int): Position on x-axis
+            y (int): Position on y-axis
+        """
         super().__init__()
         self.image = pygame.Surface([block_width,block_height])
         self.image.fill(color)
@@ -30,6 +36,13 @@ class Block(pygame.sprite.Sprite):
 class Ball(pygame.sprite.Sprite):
 
     def __init__(self,color,width,height):
+        """The constructor ball class
+
+        Args:
+            color (tuple): The color of the ball
+            width (int): The width of the ball
+            height (int): The height of the ball
+        """
         super().__init__()
         self.image = pygame.Surface([width, height])
         #Colors in the ball
@@ -38,34 +51,38 @@ class Ball(pygame.sprite.Sprite):
         self.velocity = [randint(4,8),randint(1,8)]
         self.rect = self.image.get_rect()
     
+
     def move(self):
         self.rect.x += self.velocity[0]
         self.rect.y += self.velocity[1]
 
     def bounce(self):
+        """This function makes the ball bounce the other way when a collision is detected
+        """
         self.velocity[0] = -self.velocity[0]
         self.velocity[1] = - self.velocity[1] + randint(-4,4)
 
-#Class of the player platform   
 class Platform(pygame.sprite.Sprite):
 
     def __init__(self):
+        """The constructor platform class
+        """
         super().__init__()
-        #Properties of platform
         self.height = 15
         self.width = 100  
         self.image = pygame.Surface([self.width, self.height])
         self.speed = 10
-        #Color of platform
         self.image.fill(black)
-        #Sets position where ball comes from
         self.rect = self.image.get_rect()
         self.screenh = pygame.display.get_surface().get_height()
         self.screenw = pygame.display.get_surface().get_width()
         self.rect.x = 0
+        #Sets platform spawn point
         self.rect.y = self.screenh-self.height
 
     def movement(self):
+        """This function makes the platform follow the mouse
+        """
         pos = pygame.mouse.get_pos()
         self.rect.x = pos[0]
         if self.rect.x > self.screenw - self.width:
@@ -79,12 +96,13 @@ screen = pygame.display.set_mode([800, 600])
 #Sets name of the game window
 pygame.display.set_caption('Breakout!')
 
+#Hides the mouse pointer
 pygame.mouse.set_visible(0)
 
 #Creates background
 bg = pygame.Surface(screen.get_size())
 
-#Sprite list
+#Sprite lists
 all_sprites = pygame.sprite.Group()
 blocks = pygame.sprite.Group()
 
@@ -101,11 +119,10 @@ all_sprites.add(ball)
 top = 80
 blocknumber = 32
 
-#Creates the block rows
+"""Creates the blocks as well as placing them in columns and rows
+"""
 for row in range(5):
-    #Creates block columns
     for column in range(0,blocknumber):
-        #Creates the blocks
         block = Block(orange, column * (block_width + 2) + 1, top)
         blocks.add(block)
         all_sprites.add(block)
@@ -117,19 +134,25 @@ run = True
 #Used to control the fps
 fps = pygame.time.Clock()
 
+#Reads highscore from file
 with open("highscore.txt", "r") as f:
     highscore = int(f.readline().strip())
 
 #Main loop
 while run:
+
     #Color of the background
     screen.fill(blue)
 
     all_sprites.update()
 
     platform.movement()
+    
     ball.move()
-
+    
+    """Detects collision between the ball and the walls and ends the game 
+    if the ball hits the bottom wall.
+    """
     if ball.rect.x>=790:
         ball.velocity[0] = -ball.velocity[0]
     if ball.rect.x<=0:
@@ -155,9 +178,12 @@ while run:
       ball.rect.y -= ball.velocity[1]
       ball.bounce()
 
-
+#Removes block if collision with ball is detected
     kill_block = pygame.sprite.spritecollide(ball, blocks, True)
 
+    """Bounces if block is hit and not all are gone. 
+    Ends game and shows win screen if all blocks are removed.
+    """
     if len(kill_block) > 0:
         ball.bounce()
         score += 1
@@ -170,7 +196,9 @@ while run:
 
             run = False
 
-
+        """If the score achieved is larger than the current highscore it 
+        is written to a file and saved
+        """
     for event in pygame.event.get():
         if score >= highscore:
             with open("highscore.txt", "w") as f:
@@ -180,20 +208,25 @@ while run:
 
             run = False
     
+        """Colors the background and draws the line at the top of the screen
+        """
     screen.fill(blue)
     pygame.draw.line(screen, white, [0, 38], [800, 38], 2)
 
-
+#Displays the score and high score
     font = pygame.font.Font(None, 34)
     text = font.render("Score: " + str(score), 1, white)
     screen.blit(text, (20,10))
     text = font.render("High Score: " + str(highscore), 1, white)
     screen.blit(text, (500, 10))
 
-    all_sprites.draw(screen)
-    
+    """Draws all sprites on screen and displays them
+    """
+    all_sprites.draw(screen)    
     pygame.display.flip()
 
-    fps.tick(60)
+#Sets the games fps
+    fps.tick(40)
+
 #Ends the program
 pygame.quit()
